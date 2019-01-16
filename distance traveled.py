400f84fd9472fa261aa4da566e5061ec0c1aaaf0
hurshy <33412682+hurshy@users.noreply.github.com>
2019-01-16 13:27:23 -0600

    Add files via upload

---
+++ distance traveled.py
@@ -0,0 +1,16 @@
+#Tyler Hegewald Distance Traveled
+#Distance = speed * time
+#Calculate the Distance for 6 hours
+distanceOne = 70 * 6
+
+#Calculate the Distance for 10 hours
+distanceTwo = 70 * 10
+
+#calculate the distance for 15 hours
+distanceThree = 70 * 15
+
+#Display the 3 calculations
+print("Distance in;")
+print("6 hours", distanceOne)
+print("10 hours", distanceTwo)
+print("15 hours", distanceThree)
